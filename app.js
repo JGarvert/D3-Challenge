82bed418d9cf07c93f12ffd0787af7e7bf523732
JGarvert <julie.garvert@adidas.com>
2021-04-18 08:39:45 -0700

    changed file structure

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Import Data
-d3.csv("assets/data/data.csv").then(avgageData => {
+d3.csv("data.csv").then(avgageData => {
 
     // Parse Data and cast as numbers
    avgageData.forEach(data => {
